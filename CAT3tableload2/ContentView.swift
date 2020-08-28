5df7dc3ec19c24b1a37305facf308443a7b75486
John Wager <jwager0408@yahoo.ca>
2020-08-28 11:25:40 -0400

    Initial Commit

---
+++ CAT3tableload2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CAT3tableload2
+//
+//  Created by John Wager on 2020-08-28.
+//  Copyright © 2020 Wager and Associates. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
